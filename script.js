61d8ad0464a7d1129c5cc3c11fc290fb063f92d8
小林砂緒 <123369262+suna-0512@users.noreply.github.com>
2024-10-24 19:14:47 +0900

    カメラ機能を試した

---
+++ script.js
@@ -0,0 +1,44 @@
+
+async function main () {
+  try {
+		//HTMLから取得
+    const video = document.querySelector('#video') // <1>
+    const button = document.querySelector('#button')
+    const image = document.querySelector('#image')
+
+		//ここでユーザーのデバイスカメラからの情報を取得
+    const stream = await navigator.mediaDevices.getUserMedia({ // <2>
+      video: {
+        facingMode: 'user',   //内カメ?
+        //facingMode: 'environment',  //外カメ?
+      },
+      audio: false,
+    })
+
+		//
+    video.srcObject = stream // <3>		//メディアソースを提供するオブジェクトを設定または取得
+
+    const [track] = stream.getVideoTracks()
+    const settings = track.getSettings()
+    const {width, height} = settings // <4>
+
+
+		//撮影ボタンが押されたときの処理
+    button.addEventListener('click', event => { // <5>
+      const canvas = document.createElement('canvas') // <6>
+      canvas.setAttribute('width', width)	//id=canvasの要素の、widthという属性をwidth(変数)に指定する
+      canvas.setAttribute('height', height)
+
+      const context = canvas.getContext('2d')
+      context.drawImage(video, 0, 0, width, height) // <7>
+
+      const dataUrl = canvas.toDataURL('image/jpeg') // <8>
+      image.setAttribute('src', dataUrl) // <9>
+    })
+
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+main()
